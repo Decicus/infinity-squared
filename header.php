344d0e89b2f21ab6cffa4b4e82ca26c68bd38514
Alex Thomassen <alex@thomassen.xyz>
2018-01-10 12:05:53 +0100

    Update favicons

--- header.php
+++ header.php
@@ -30,25 +30,23 @@ yourls_load_custom_textdomain( 'isq_translation', 'public/languages' );
 			<link rel="stylesheet" href="<?php echo YOURLS_SITE; ?>/public/custom.css" /><!-- Custom CSS -->
 		<?php } ?>
 
-		<!-- App icons generated using http://realfavicongenerator.net -->
-		<link rel="apple-touch-icon" sizes="57x57" href="public/images/app-icons/apple-touch-icon-57x57.png">
-		<link rel="apple-touch-icon" sizes="60x60" href="public/images/app-icons/apple-touch-icon-60x60.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="public/images/app-icons/apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="public/images/app-icons/apple-touch-icon-76x76.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="public/images/app-icons/apple-touch-icon-114x114.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="public/images/app-icons/apple-touch-icon-120x120.png">
-		<link rel="apple-touch-icon" sizes="144x144" href="public/images/app-icons/apple-touch-icon-144x144.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="public/images/app-icons/apple-touch-icon-152x152.png">
-		<link rel="apple-touch-icon" sizes="180x180" href="public/images/app-icons/apple-touch-icon-180x180.png">
-		<link rel="icon" type="image/png" href="public/images/app-icons/favicon-32x32.png" sizes="32x32">
-		<link rel="icon" type="image/png" href="public/images/app-icons/android-chrome-192x192.png" sizes="192x192">
-		<link rel="icon" type="image/png" href="public/images/app-icons/favicon-96x96.png" sizes="96x96">
-		<link rel="icon" type="image/png" href="public/images/app-icons/favicon-16x16.png" sizes="16x16">
-		<link rel="manifest" href="public/images/app-icons/manifest.json">
-		<link rel="shortcut icon" href="public/images/app-icons/favicon.ico">
-		<meta name="msapplication-TileColor" content="#2b5797">
-		<meta name="msapplication-TileImage" content="public/images/app-icons/mstile-144x144.png">
-		<meta name="msapplication-config" content="public/images/app-icons/browserconfig.xml">
+		<link rel="apple-touch-icon" sizes="57x57" href="/images/icons/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="/images/icons/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/images/icons/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="/images/icons/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/images/icons/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/images/icons/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/images/icons/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/images/icons/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="/images/icons/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="/images/icons/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png">
+		<link rel="manifest" href="/images/icons/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="/images/icons/ms-icon-144x144.png">
+		<meta name="msapplication-config" content="/images/icons/browserconfig.xml">
 		<meta name="theme-color" content="#013f6d">
 	</head>
 
